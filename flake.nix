02e312315703f423c76187b7c35c21db2df6a502
xenolf <xenolf@users.noreply.github.com>
2023-02-05 23:46:34 +0100

    Update devenv and add new packages

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     nixgl.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     # Devenv
-    devenv.url = "github:cachix/devenv/v0.5";
+    devenv.url = "github:cachix/devenv/v0.5.1";
 
     # TODO: Add any other flake you might need
     # hardware.url = "github:nixos/nixos-hardware";
